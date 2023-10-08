6f5c2d764e5a68adf9ed429214757974b7427ad9
tajaa <tajatheprince@gmail.com>
2023-10-07 17:49:36 -0700

    fixed the topic list to dynamically grab information from debatase and insert it where I want to in the blog post

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -3,27 +3,48 @@ import RemoveBtn from "./RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-function TopicsList() {
+//get topics from db
+const getTopics = async () => {
+  try {
+    //by default next.js stores the first data so use 'no store'
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("failed to fetch topics");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("error loading topics:", error);
+  }
+};
+
+export default async function TopicsList() {
+  //call 'getTopics' here
+  const { topics } = await getTopics();
+
   return (
     <>
-      <div
-        className="p-4 border border-slate-300 my-3 flex
+      {/*map through topics and display them*/}
+      {topics.map((t) => (
+        <div
+          className="p-4 border border-slate-300 my-3 flex
       justify-between gap-5 items-start"
-      >
-        <div>
-          <h2 className="font-bold text-2xl font-serif">Topioc Title</h2>
-          <div>Topic Description</div>
-        </div>
+        >
+          <div>
+            <h2 className="font-bold text-2xl font-serif">{t.title}</h2>
+            <div>{t.description}</div>
+          </div>
 
-        <div className="flex gap-2">
-          <RemoveBtn />
-          <Link href={"/editTopic/123"}>
-            <HiPencilAlt size={24} />
-          </Link>
+          <div className="flex gap-2">
+            <RemoveBtn />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 }
-
-export default TopicsList;
