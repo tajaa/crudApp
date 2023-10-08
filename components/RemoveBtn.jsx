78014f2966d121bf162e5cbb08e1f2b04796d290
tajaa <tajatheprince@gmail.com>
2023-10-08 10:49:44 -0700

    added logic to remove a post from the page

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,8 +1,18 @@
+"use client";
 import { HiOutlineTrash } from "react-icons/hi";
 
-export default function RemoveBtn() {
+export default function RemoveBtn({ id }) {
+  const removeTopic = async () => {
+    const confirmed = confirm("are you sure?");
+
+    if (confirmed) {
+      await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
+    }
+  };
   return (
-    <button className="text-red-300">
+    <button onClick={removeTopic} className="text-red-300">
       <HiOutlineTrash size={24} />
     </button>
   );
